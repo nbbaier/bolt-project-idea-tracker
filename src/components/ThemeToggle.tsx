c586e09e20bdf1a958af559754b05ff08c7c9fc4
Nico Baier <nico.baier@gmail.com>
2025-08-31 02:14:56 -0500

    Make theme toggle icon slightly larger

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -15,8 +15,8 @@ export function ModeToggle() {
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
 				<Button variant="outline" size="icon">
-					<Sun className="w-5 h-5 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute w-5 h-5 transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100" />
+					<Sun className="w-6 h-6 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0" />
+					<Moon className="absolute w-6 h-6 transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100" />
 					<span className="sr-only">Toggle theme</span>
 				</Button>
 			</DropdownMenuTrigger>
