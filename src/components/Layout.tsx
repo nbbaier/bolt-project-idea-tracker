e7d9efebc1ceef4b645da1f37c6eebe542b51762
Nico Baier <nico.baier@gmail.com>
2025-08-31 02:18:00 -0500

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
 		<div className="min-h-screen font-sans bg-background">
-			<div className="px-4 mx-auto w-full max-w-4xl sm:px-6 lg:px-8">
+			<div className="px-4 mx-auto w-full max-w-3xl sm:px-6 lg:px-8">
 				{children}
 			</div>
 		</div>
