c3588402d3a296a9ebf35054e6421c568bfc910e
Nico Baier <nico.baier@gmail.com>
2025-09-06 02:01:59 -0500

    Updated IdeaCard.tsx

--- src/components/IdeaCard.tsx
+++ src/components/IdeaCard.tsx
@@ -122,7 +122,6 @@ export const IdeaCard: React.FC<IdeaCardProps> = ({
 						</Button>
 					</DialogTrigger>
 					<DialogContent className="sm:max-w-[500px] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] duration-200">
-					<DialogContent className="sm:max-w-[500px] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] duration-300">
 						<DialogHeader>
 							<DialogTitle>Edit Project Idea</DialogTitle>
 						</DialogHeader>
