13e715d681528400c42a557891a1a549a9741370
Nico Baier <nico.baier@gmail.com>
2025-09-06 02:01:08 -0500

    Add smooth animations to dialog

--- src/components/AddIdeaCard.tsx
+++ src/components/AddIdeaCard.tsx
@@ -73,7 +73,7 @@ export const AddIdeaCard: React.FC = () => {
 			</Card>
 
 			<Dialog open={isOpen} onOpenChange={setIsOpen}>
-				<DialogContent className="sm:max-w-[500px]">
+				<DialogContent className="sm:max-w-[500px] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] duration-200">
 					<DialogHeader>
 						<DialogTitle>Add New Project Idea</DialogTitle>
 					</DialogHeader>
